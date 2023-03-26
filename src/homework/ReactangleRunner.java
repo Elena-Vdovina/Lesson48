f3cba71840e1bc564dd8a36194605b4bed400578
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 10:55:36 +0200

    Add main()

--- src/homework/ReactangleRunner.java
+++ src/homework/ReactangleRunner.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReactangleRunner {
+  public static void main(String[] args) {
+    List<Rectangle> rectangles=new ArrayList<>();
+    rectangles.add(new Rectangle(5,6));
+    rectangles.add(new Rectangle(2, 15));
+    rectangles.add(new Rectangle(7,6));
+    rectangles.add(new Rectangle(4,5));
 
+    rectangles.sort(new RectangleSquareComparator());
+
+    for (Rectangle r:rectangles){
+      System.out.println(r);
+    }
   }
 }
