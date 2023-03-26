eed9833f7211e839cd211535483d7a90d3121389
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 15:29:23 +0200

    Change main()

--- src/homework/StampRunner.java
+++ src/homework/StampRunner.java
@@ -1,5 +1,8 @@
 package homework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StampRunner {
   public static void main(String[] args) {
     Timestamp t1=new Timestamp(6, 5);
@@ -12,5 +15,17 @@ public class StampRunner {
     t1.addMinutes(6);
     System.out.println(t1);
 
+    List<Timestamp> stamps=new ArrayList<>();
+    stamps.add(new Timestamp(10,20));
+    stamps.add(new Timestamp(10,30));
+    stamps.add(new Timestamp(20,30));
+    stamps.add(new Timestamp(5,30));
+    stamps.add(new Timestamp(10,10));
+
+    stamps.sort(new TimestampComparator());
+
+    for (Timestamp t:stamps){
+      System.out.println(t);
+    }
   }
 }
