92504914aeeec21336479f0bc623aac8eedb7220
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 16:52:09 +0200

    add addHoursCheck2()

--- src/homework/TimestampTests.java
+++ src/homework/TimestampTests.java
@@ -59,12 +59,21 @@ public class TimestampTests {
     assertEquals("01:05", t1.getTimeStamp());
   }
 
-  @DisplayName("Проверяем добавление часов до суток")
+  @DisplayName("Проверяем добавление часов <24")
   @Test
   public void addHoursCheck1() {
     Timestamp t1 = new Timestamp(1, 1);
     t1.addHours(5);
     assertEquals(6, t1.getHours());
     assertEquals("06:01", t1.getTimeStamp());
+  }
+
+  @DisplayName("Проверяем добавление часов =24")
+  @Test
+  public void addHoursCheck2() {
+    Timestamp t1 = new Timestamp(10, 1);
+    t1.addHours(14);
+    assertEquals(0, t1.getHours());
+    assertEquals("00:01", t1.getTimeStamp());
   }
 }
