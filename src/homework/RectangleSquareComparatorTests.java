66192e6714dc28b9d3ff5f2617cee8f550aeab36
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 11:08:19 +0200

    Create RectangleSquareComparatorTests

---
+++ src/homework/RectangleSquareComparatorTests.java
@@ -0,0 +1,10 @@
+package homework;
+
+import org.junit.jupiter.api.Test;
+
+// Создать класс Rectangle (прямоугольник), которому в конструкторе нужно передавать длину и ширину.
+// Написать компаратор, который сравнивает прямоугольники по площади.
+// Написать тесты для этого компаратора.
+public class RectangleSquareComparatorTests {
+
+}
