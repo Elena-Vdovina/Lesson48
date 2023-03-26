5131159dbf4a0d653fb2df98d57bd8ea89c4ce80
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 16:28:04 +0200

    add negativHorus()

--- src/homework/TimestampTests.java
+++ src/homework/TimestampTests.java
@@ -1,4 +1,16 @@
 package homework;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import register.ReceiptLine;
+
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
 public class TimestampTests {
+  @DisplayName("Ловим некорректный ввод часов <0")
+  @Test
+  public void negativeHours(){
+    assertThrowsExactly(IllegalArgumentException.class, () -> new Timestamp(-1, 1));
+  }
+
 }
