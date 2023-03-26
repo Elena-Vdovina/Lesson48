6927fb8be21ccf4c38dbebdd0f13f63bdc03da0c
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 13:09:16 +0200

    Change constructor, add checkHours(), checkMinutes

--- src/homework/Timestamp.java
+++ src/homework/Timestamp.java
@@ -10,4 +10,39 @@ package homework;
 // Попытка добавить 1 минуту к отметке 23:59 должна приводить к установке времени на отметку 00:00.
 // Написать тесты.
 public class Timestamp {
+
+  private String timeStamp;
+  private int hours;
+  private int minutes;
+
+  public Timestamp(int hours, int minutes) {
+    checkHours(hours);
+    checkMinutes(minutes);
+    if (hours < 10) {
+      this.timeStamp = "0" + this.hours + ":";
+    } else {
+      this.timeStamp = this.hours + ":";
+    }
+    if (minutes < 10) {
+      this.timeStamp += "0" + this.minutes;
+    } else {
+      this.timeStamp += this.minutes;
+    }
+  }
+
+  private void checkHours(int hours) {
+    if (hours < 0 || hours > 23) {
+      throw new IllegalArgumentException("Некорректное значение часов " + hours);
+    }
+    this.hours = hours;
+  }
+
+  private void checkMinutes(int minutes) {
+    if (minutes < 0 || minutes > 59) {
+      throw new IllegalArgumentException("Некорректное значение минут " + minutes);
+    }
+    this.minutes = minutes;
+  }
+
+
 }
