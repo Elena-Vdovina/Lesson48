03f493425a0e24a40840d041eb9ff40ba9967c98
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 15:29:01 +0200

    Override compare()

--- src/homework/TimestampComparator.java
+++ src/homework/TimestampComparator.java
@@ -5,4 +5,12 @@ import java.util.Comparator;
 // В классе должен быть реализован интерфейс Comparable, который бы сравнивал отметки времени
 // (раньше - меньше, позже - больше).
 public class TimestampComparator implements Comparator<Timestamp> {
+
+  @Override
+  public int compare(Timestamp t1, Timestamp t2){
+    if (t1.getHours()!= t2.getHours()){
+      return t1.getHours()-t2.getHours();
+    }
+    return t1.getMinutes()- t2.getMinutes();
+  }
 }
