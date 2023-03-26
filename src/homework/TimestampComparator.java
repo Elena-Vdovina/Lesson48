0d1d21480064db9e04575bb24a5409b92796e51e
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 15:20:01 +0200

    Add TimestampComparator

---
+++ src/homework/TimestampComparator.java
@@ -0,0 +1,8 @@
+package homework;
+
+import java.util.Comparator;
+
+// В классе должен быть реализован интерфейс Comparable, который бы сравнивал отметки времени
+// (раньше - меньше, позже - больше).
+public class TimestampComparator implements Comparator<Timestamp> {
+}
