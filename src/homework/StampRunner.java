cb50b23f46f94e6637dc4b9d88e6f370d1c77faf
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 15:15:57 +0200

    Add main()

--- src/homework/StampRunner.java
+++ src/homework/StampRunner.java
@@ -1,5 +1,16 @@
 package homework;
 
 public class StampRunner {
+  public static void main(String[] args) {
+    Timestamp t1=new Timestamp(6, 5);
+    System.out.println(t1);
+    t1.setHours(10);
+    t1.setMinutes(54);
+    System.out.println(t1);
+    t1.addHours(20);
+    System.out.println(t1);
+    t1.addMinutes(6);
+    System.out.println(t1);
 
+  }
 }
