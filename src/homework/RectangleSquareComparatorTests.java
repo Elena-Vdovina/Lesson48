7fbafce76f00f0f2f7924b20d20f4bd53720cf4e
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 11:37:08 +0200

    Add sort()

--- src/homework/RectangleSquareComparatorTests.java
+++ src/homework/RectangleSquareComparatorTests.java
@@ -3,8 +3,11 @@ package homework;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // Создать класс Rectangle (прямоугольник), которому в конструкторе нужно передавать длину и ширину.
@@ -51,5 +54,30 @@ public class RectangleSquareComparatorTests {
 
     assertTrue(result1 < 0);
     assertTrue(result2 > 0);
+  }
+
+  @DisplayName("Проверяем сортировку")
+  @Test
+  public void sort() {
+    Rectangle r1 = new Rectangle(2, 2);
+    Rectangle r2 = new Rectangle(2, 1);
+    Rectangle r3 = new Rectangle(1, 2);
+    Rectangle r4 = new Rectangle(1, 1);
+
+    List<Rectangle> actual = new ArrayList<>();
+    actual.add(r1);
+    actual.add(r2);
+    actual.add(r3);
+    actual.add(r4);
+
+    List<Rectangle> expected = new ArrayList<>();
+    expected.add(r4);
+    expected.add(r3);
+    expected.add(r2);
+    expected.add(r1);
+
+    actual.sort(comparator);
+
+    assertEquals(expected, actual);
   }
 }
