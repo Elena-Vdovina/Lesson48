3ca0829fb26ec6036077ce18553adebf32cc83c7
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 10:34:30 +0200

    Add fields, constructor, getters

--- src/homework/Rectangle.java
+++ src/homework/Rectangle.java
@@ -5,5 +5,25 @@ package homework;
 // Написать тесты для этого компаратора.
 public class Rectangle {
 
+  private final int length;
+  private final int width;
+  private final int square;
 
+  public Rectangle(int length, int width) {
+    this.length = length;
+    this.width = width;
+    this.square = length * width;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public int getSquare() {
+    return square;
+  }
 }
