d38c40109f7b2a91a98b3e898c0125166db20d9b
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-26 15:58:48 +0200

    Add differentStamp

--- src/homework/TimestampComparatorTests.java
+++ src/homework/TimestampComparatorTests.java
@@ -1,4 +1,25 @@
 package homework;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TimestampComparatorTests {
+  private final Comparator<Timestamp> comparator = new TimestampComparator();
+
+  @DisplayName("Сравниваем разные точки времени")
+  @Test
+  public void differentStamp() {
+    Timestamp t1 = new Timestamp(1, 1);
+    Timestamp t2 = new Timestamp(2, 2);
+
+    int result1 = comparator.compare(t1, t2);
+    int result2 = comparator.compare(t2, t1);
+
+    assertTrue(result1 < 0);
+    assertTrue(result2 > 0);
+  }
 }
